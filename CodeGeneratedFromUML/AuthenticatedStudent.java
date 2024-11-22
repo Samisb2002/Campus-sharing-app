80095bb3d4bcf2599495687f599b86f0784bcae4
Ineednewusernamee <150426133+Ineednewusernamee@users.noreply.github.com>
2024-11-22 13:59:29 +0100

    update

--- CodeGeneratedFromUML/AuthenticatedStudent.java
+++ CodeGeneratedFromUML/AuthenticatedStudent.java
@@ -1,11 +1,11 @@
 public class AuthenticatedStudent extends User implements Observer {
     public ScoreManager scoreManager;
+    private Profile profile;
 
     public AuthenticatedStudent(Integer userId, String name, String email, String password) {
         super(userId, name, email, password);
         this.profile = new Profile(this); 
     }
-
 
     public Profile getProfile() {
         return profile;
