2304924fbfa57de052fc0c14163206e45879979d
Ayoub Mohammed Zouhri <Ayoub-mohammed.zouhri@etu.univ-nantes.fr>
2024-11-22 14:19:55 +0100

    update AuthenticatedStudent

--- CodeGeneratedFromUML/AuthenticatedStudent.java
+++ CodeGeneratedFromUML/AuthenticatedStudent.java
@@ -1,23 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuthenticatedStudent extends User implements Observer {
     public ScoreManager scoreManager;
     private Profile profile;
+    public List<Product> products= new ArrayList<>();
 
     public AuthenticatedStudent(Integer userId, String name, String email, String password) {
         super(userId, name, email, password);
-        this.profile = new Profile(this); 
+        this.profile = new Profile(this);
     }
 
     public Profile getProfile() {
         return profile;
     }
 
-    public void requestProduct() {
+    public void requestProduct(String productName) {
         System.out.println("Product requested by: " + this.getUserName());
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(productName)) {
+                System.out.println("Product found: " + product.getName());
+                break;
+            }
+        }
     }
 
 
     public void postProduct(Product product) {
         System.out.println("Product posted: " + product.getName() + " by " + this.getUserName());
+        products.add(product);
     }
 
     @Override
