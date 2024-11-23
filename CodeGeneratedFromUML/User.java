525eb621af6f5d9ce467199760b2d52da55a621a
Ayoub Mohammed Zouhri <Ayoub-mohammed.zouhri@etu.univ-nantes.fr>
2024-11-23 04:02:55 +0100

    Commit of User.java

--- CodeGeneratedFromUML/User.java
+++ CodeGeneratedFromUML/User.java
@@ -1,13 +1,11 @@
 public class User {
     protected Integer userId;
-
     protected String name;
-
     protected String email;
-
     protected String password;
 
-    public User(Integer userId, String name, String email, String password) {
+    public User(Integer userId, String name, String email,
+                String password) {
         this.userId = userId;
         this.name = name;
         this.email = email;
@@ -22,14 +20,11 @@ public class User {
         return name;
     }
 
-
     public String getUserEmail() {
         return email;
     }
 
-
     public String getPassword() {
         return password;
     }
-
 }
